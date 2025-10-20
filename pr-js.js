18f4756c054f869ba2777f199c74366ece7ed4c8
Jommel Pastoral <116055436+qkyuuu@users.noreply.github.com>
2025-10-20 17:27:55 +0800

    Update pr-js.js

--- pr-js.js
+++ pr-js.js
@@ -57,10 +57,41 @@ setupToggle(6);
 document
   .getElementById("confirmProceedBtn")
   .addEventListener("click", function () {
-    // Open the pr_feedback.php in a new tab
-    window.open("pr-feedback/pr_feedback.php", "_blank");
+    // Find the form element
+    var form = document.querySelector("form");
+
+    // Make sure the form is ready for submission
+    if (form) {
+      // We can manually submit the form here
+      form.submit(); // This will trigger the backend (submit_review.php) to save data
+    }
+
+    // Close the modal after submitting the form
+    var modal = new bootstrap.Modal(
+      document.getElementById("confirmationModal")
+    );
+    modal.hide();
   });
 
+// EMAILING THE LINK - (Open Outlook with a pre-filled email)
+function sendEmail(prId) {
+  let bodyText =
+    `Peer Review Answers:\n\n` +
+    `Hope you're doing well!\n\n` +
+    `Task Name: Sample task name\n\n` +
+    `I've noticed that there are some errors\n` +
+    `For reference, here is the PRID: ${prId}\n\n` +
+    `Here is the link to the feedback page: http://localhost/EVENTS/EVENT-PR/pr-feedback/pr_feedback.php?pr_id=${prId}\n\n` +
+    `Thank you so much!`;
+
+  const subject = encodeURIComponent("Peer Review Submission");
+  const body = encodeURIComponent(bodyText);
+
+  // Opens the default email client (Outlook or others)
+  window.location.href = `mailto:v-jopastoral@microsoft.com?subject=${subject}&body=${body}`;
+}
+
+// // EMAILING THE FEEDBACK ITSELF
 // function sendEmail() {
 //   const maxQuestions = 80; // total questions
 //   let bodyText = "Peer Review Answers:\n\n";
